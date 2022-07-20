0265dabdc779290dc596792ac822bdaaa28011cf
Bruno Urbán <brurban99@gmail.com>
2022-07-20 15:38:43 +0200

    Add files via upload

---
+++ Basics:Schemas&Queries/Live Reload for GraphQL-Yoga/src/index.js
@@ -0,0 +1,44 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+// Scalar types - String, Boolean, Int, Float, ID
+
+// Type definitions (schema)
+const typeDefs = `
+    type Query {
+        title: String!
+        price: Float!
+        releaseYear: Int
+        rating: Float
+        inStock: Boolean!
+    }
+`
+
+// Resolvers
+const resolvers = {
+    Query: {
+        title() {
+            return 'The War of Art'
+        },
+        price() {
+            return 12.99
+        },
+        releaseYear() {
+            return 2008
+        },
+        rating() {
+            return 5
+        },
+        inStock() {
+            return true
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('The server is up!')
+})
